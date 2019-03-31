00eb3f9732766e7a5779fb1492e1c1a3df4ba606
TaekSoo <gjcka1234@naver.com>
2019-03-31 19:14:11 +0900

    2019.03.31

---
+++ Practice/Ch6/분할정복/1부터n까지의합.h
@@ -0,0 +1,12 @@
+#pragma once
+
+int fastSum(int n)
+{
+	// ±âÀú »ç·Ê
+	if (n == 1)
+		return 1;
+	if (n % 2 == 1)
+		return fastSum(n - 1) + n;
+
+	return 2 * fastSum(n / 2) + (n / 2) * (n / 2);
+}
